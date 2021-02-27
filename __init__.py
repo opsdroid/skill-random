5bed00cbbb9a969fa3ede009b29fe51c0800c01e
Joel Bennett <Jaykul@HuddledMasses.org>
2021-02-27 01:02:44 -0500

    A better die roller
    Fix die roller

--- __init__.py
+++ __init__.py
@@ -1,13 +1,6 @@
 from opsdroid.matchers import match_regex
-import random
-
-
-DICE_ROLL_RESPONSES = [
-    "The dice says {number}",
-    "You rolled a {number}",
-    "It's a {number}"
-]
-
+from random import choice
+from d20 import roll, SimpleStringifier
 
 COIN_FLIP_RESPONSES = [
     "It landed on {result}",
@@ -15,16 +8,19 @@ COIN_FLIP_RESPONSES = [
     "It's {result}"
 ]
 
+@match_regex(r'roll a dic?e', case_sensitive=False)
+async def roll_die(opsdroid, config, message):
+    await message.respond("You rolled {}".format(roll("1d6").result))
 
-@match_regex(r'roll a dice', case_sensitive=False)
-async def roll_a_dice(opsdroid, config, message):
-    number = random.randint(1,6)
-    text = random.choice(DICE_ROLL_RESPONSES).format(number=number)
-    await message.respond(text)
-
+@match_regex(r'roll( a)? (?P<dice>\d*d\d+.*)', case_sensitive=False)
+async def roll_dice(opsdroid, config, message):
+    try:
+        await message.respond("You rolled {}".format(roll(message.regex.group('dice'),stringifier=SimpleStringifier())))
+    except:
+        await message.respond("Sorry, did you mean to roll dice? Try 'roll a d6' or 'roll 3d8+2'")
 
 @match_regex(r'flip a coin', case_sensitive=False)
 async def flip_a_coin(opsdroid, config, message):
-    result = random.choice(["heads", "tails"])
-    text = random.choice(COIN_FLIP_RESPONSES).format(result=result)
+    result = choice(["heads", "tails"])
+    text = choice(COIN_FLIP_RESPONSES).format(result=result)
     await message.respond(text)
